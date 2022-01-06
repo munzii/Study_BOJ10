ee27a99ba9e2b418b0698dbc8b9992897214a2c6
munzii <munzii422@gmail.com>
2022-01-06 23:45:46 +0900

    studying

--- src/P2447.java
+++ src/P2447.java
@@ -5,13 +5,21 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class P2447 {
+	static char[][] arr;
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int n = Integer.parseInt(br.readLine());
+		arr = new char[n][n];
+		
+		
 
+	}
+	
+	static void s(int x, int y, int n, boolean black) {
+		
 	}
 
 }
