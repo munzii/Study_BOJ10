cf221dab4136209c1071b6c57a0572c68fb15e8e
munzii <munzii422@gmail.com>
2022-01-08 22:04:00 +0900

    studying

--- src/P2447.java
+++ src/P2447.java
@@ -14,11 +14,49 @@ public class P2447 {
 		int n = Integer.parseInt(br.readLine());
 		arr = new char[n][n];
 		
+		s(0,0,n,false);
 		
+		for(int i=0; i<n; i++) {
+			bw.write(arr[i]);
+			bw.write("\n");
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
 
 	}
 	
 	static void s(int x, int y, int n, boolean black) {
+		
+		if(black) {
+			for(int i=x; i<x+n; i++) {
+				for(int j=y; j<y+n; j++) {
+					arr[i][j] = ' ';
+				}
+			}
+			return;
+		}
+		
+		if(n==1) {
+			arr[x][y] = '*';
+			return;
+		}
+		
+		int size = n/3;
+		int count =0;
+		
+		for(int i=x; i<x+n; i+=size) {
+			for(int j=y; j<y+n; j+=size) {
+				count++;
+				if(count==5) {
+					s(i,j,size,true);
+				}
+				else {
+					s(i,j,size,false);
+				}
+			}
+		}
 		
 	}
 
